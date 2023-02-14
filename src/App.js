8e167292aed57d61b054368a6dc3717f7321897e
Vasco-Mendes017 <vasco.mendes017@gmail.com>
2023-02-14 12:41:14 +0000

    something wrong with import images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import NavBar from './NavBar';
+import NavBar from './Components/NavBar';
 
 
 function App() {
   return (
+    
     <div className="App">
+      <h1>hello</h1>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from './src/Images/Bee_Watching_Logo.png';
+
+function NavBar(){
+    return(
+        <nav>
+            <img src={logo} alt="bee logo"/>
+        </nav>
+    )
+    
+}
+export default NavBar;
