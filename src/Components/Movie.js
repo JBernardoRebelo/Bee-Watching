6dd0a64ac6c9ab68b65824962aa665e149e1c8a2
JBernardoRebelo <bernasfasttaker@gmail.com>
2023-02-15 15:26:24 +0000

    add some styling

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Info from "./Info";
 
 // Movie object
@@ -8,39 +8,36 @@ function Movie({isCurrent}){
 
     const [listIndex, setListIndex] = useState(0);
     
-    var showMovie = 0;
     const setCurrentMovie= (index)=>{ setListIndex(index)
         console.log(index)
-    }	
-    
+    }	  
     
     return(
         <div>
-            <div className="movie">
+            <div className="currentMovie">
                 {console.log("entrei no current movie")}
                 {/* SHOWCASE ALL INFO */}
-                <img src={Info[listIndex].coverImg} height={300} width={200} alt="Movie Cover"/>
-                <p>Title: {Info[listIndex].title}</p>
-                <p>Movie Picker: {Info[listIndex].moviePicker}</p>
-                <p>Genre: {Info[listIndex].genres}</p>
-                <p>Duration: {Info[listIndex].duration}</p>
-                <p>Rate: {Info[listIndex].rate}</p>
-            </div>
-               
+                <img src={Info[listIndex].coverImg} className="movieCover" height={300} width={200} alt="Movie Cover"/>
+                <div className="currentMovieInfo">
+                    <p>Title: {Info[listIndex].title}</p>
+                    <p>Movie Picker: {Info[listIndex].moviePicker}</p>
+                    <p>Genre: {Info[listIndex].genres}</p>
+                    <p>Duration: {Info[listIndex].duration}</p>
+                    <p>Rate: {Info[listIndex].rate}</p>
+                </div>
+            </div>   
             <div className="previousMovies">
                     {console.log("entrei no previous movie")}
                     {Info.map((item, index) => {
-                        return(
-                            <div className="movie" onClick={()=> setCurrentMovie(index) }>
-                                <img src={item.coverImg} height={300} width={200} alt="Movie Cover"/>
-                                <p>Title: {item.title}</p>
-                                <p>Movie Picker: {item.moviePicker}</p>
-                            </div>
-                            
-                        )
+                    return(
+                        <div className="moviePreviousMovie" onClick={()=> setCurrentMovie(index) }>
+                            <img src={item.coverImg} className="movieCover" height={300} width={200} alt="Movie Cover"/>
+                            <p>Title: {item.title}</p>
+                            <p>Movie Picker: {item.moviePicker}</p>
+                        </div>        
+                    )
                     })}
             </div>
-                
         </div>
     )
 }
